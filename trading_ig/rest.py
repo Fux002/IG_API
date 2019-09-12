3e1447c079d35590fe1472363a1a71f1cf511f75
Fux002 <f.vermorken@gmail.com>
2019-09-12 12:49:54 +0100

    gs

--- trading_ig/rest.py
+++ trading_ig/rest.py
@@ -492,7 +492,7 @@ class IGService:
         params = {
             'limitLevel': limit_level,
             'stopLevel': stop_level,
-            'trailingStop' : 'false',
+            'trailingStop' : "false",
             'trailingStopDistance' : None,
             'trailingStopIncrement' : None
             #'guaranteedStop': guaranteed_stop
