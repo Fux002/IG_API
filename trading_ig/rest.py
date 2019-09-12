f1a055f277228ca7a78f20ed6101f77e4738fee7
Fux002 <f.vermorken@gmail.com>
2019-09-12 15:04:53 +0100

    gstop

--- trading_ig/rest.py
+++ trading_ig/rest.py
@@ -519,8 +519,8 @@ class IGService:
             'trailingStop' : "false",
             'trailingStopDistance' : None,
             'trailingStopIncrement' : None
-            #'guaranteedStop': guaranteed_stop,
-            #'stopDistance': stop_distance
+            'guaranteedStop': guaranteed_stop,
+            'stopDistance': stop_distance
         }
         url_params = {
             'deal_id': deal_id
