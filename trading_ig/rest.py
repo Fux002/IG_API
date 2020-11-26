4284b0a0336f3178f743156d61b62cc3ceccff9e
Fux002 <f.vermorken@gmail.com>
2020-11-26 14:50:00 +0000

    error handling switching

--- trading_ig/rest.py
+++ trading_ig/rest.py
@@ -1283,7 +1283,29 @@ class IGService:
         except exception as e:
             print(str(e))
         data = self.parse_response(response.text)
-        return data
+
+        if response.status_code == 200:
+
+            data = self.parse_response(response.text)
+            self.ig_session = data # store IG session
+            return data, response.status_code
+
+        elif response.status_code == 400 or \
+            response.status_code == 401 or \
+            response.status_code == 500 or \
+            response.status_code == 504:
+
+            # Raise a token exception
+            raise TokenException(response.text)
+
+        elif response.status_code == 403:
+
+            # Raise a key allowance exception
+            raise KeyAllowanceException(response.text)
+
+        else:
+            #return response.text
+            raise DealingException(response.text) 
 
     ############ END ############
 
